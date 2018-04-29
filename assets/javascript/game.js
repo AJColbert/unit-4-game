cd55c7167a36dade92b4739b3ea00a5e02407886
Aaron Colbert <aaron-colbert@hotmail.com>
2018-04-29 15:49:36 -0400

    added full movement and attack calcs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,91 +1,178 @@
 var characters = {
-    obiwan:{
-        hp:100,
-        attack:6,
-        counterattack:30,
-        id:1
+    obiwan: {
+        hp: 100,
+        attack: 6,
+        baseattack: 6,
+        counterattack: 30,
+        name: "Obi-Wan Kenobi",
+        id: 1
     },
-    macewindu:{
-        hp:80,
-        attack:8,
-        counterattack:25,
-        id:2
+    macewindu: {
+        hp: 80,
+        attack: 8,
+        baseattack: 8,
+        counterattack: 25,
+        name: "Mace Windu",
+        id: 2
     },
-    general:{
-        hp:120,
-        attack:5,
-        counterattack:37,
-        id:3
+    general: {
+        hp: 120,
+        attack: 5,
+        baseattack: 5,
+        counterattack: 37,
+        name: "General Grievous",
+        id: 3
     },
-    vader:{
-        hp:150,
-        attack:12,
-        counterattack:20,
-        id:4
+    vader: {
+        hp: 150,
+        attack: 12,
+        baseattack: 12,
+        counterattack: 20,
+        name: "Darth Vader",
+        id: 4
     }
 
 };
 
 console.log(Object.keys(characters)[0])
 console.log(Object.keys(characters)[1])
+currentattack = 0;
+
 
 //Character Selection
-$(".characterSelection").on("click", ".imagebox", function(){
+$(".characterSelection").on("click", ".imagebox", function ()
+{
     var htmlid = $(this).attr("id");
     console.log(htmlid);
     var characterobj = GetJSONString(htmlid);
     $(this).attr("data-obj", characterobj)
     $(this).clone().addClass("selected").appendTo($(".yourcharacter"));
-    
+
     $(this).remove();
     MoveEnemies();
 
 });
 
 //Clone all remaining Characters and move them to enemies div
-function MoveEnemies(selected){
+function MoveEnemies(selected)
+{
     $(".characterSelection").children().clone().addClass("badguy").appendTo($(".enemies"));
     $(".characterSelection").children().remove();
 }
 
 //On click to Select Enemy to Attack
-$(".enemies").on("click", ".imagebox", function(){
-    var htmlid = $(this).attr("id");
-    $(this).clone().addClass("defence").appendTo($(".defender"));
-    var characterobj = GetJSONString(htmlid);
-    $(this).attr("data-obj", characterobj)
-    $(this).remove();
+$(".enemies").on("click", ".imagebox", function ()
+{
+    //console.log($(".defender").children())
+    if ($(".defender").children().length <= 0)
+    {
+        var htmlid = $(this).attr("id");
+        var characterobj = GetJSONString(htmlid);
+        $(this).attr("data-obj", characterobj)
+        $(this).clone().addClass("defence").appendTo($(".defender"));
+        $(this).remove();
+        $("#pAttacker").text("")
+    }
 });
 
 //Attack button to trigger calls to math for hp and attack
-$(".fightsection").on("click","#attack-btn",function(){
-    //Get attacjer obj stored in element
-    var dataobj = $(".selected").attr("data-obj");
-    var attackerObj = JSON.parse(dataobj);
-    //Get defender Obj store in element
-    var dataobj2 = $(".defence").attr("data-obj");
-    var defenderObj = JSON.parse(dataobj);
-    //Calculate defendr hp - attacker attack
-    //attacker attack increases by attack power
-    //Calculate attacker hp -  defender counter attack 
-    //Display new hps
-    
+$(".fightsection").on("click", "#attack-btn", function ()
+{
+    if ($(".defender").children().length > 0)
+    {
+        //Get attacjer obj stored in element
+        var dataobj = $(".selected").attr("data-obj");
+        var attackerObj = JSON.parse(dataobj);
+        //Get defender Obj store in element
+        var dataobj2 = $(".defence").attr("data-obj");
+        var defenderObj = JSON.parse(dataobj2);
+        console.log(attackerObj.attack)
+        //Calculate defendr hp - attacker attack
+        defenderObj.hp = defenderObj.hp - attackerObj.attack;
+
+        //Calculate attacker hp -  defender counter attack 
+        attackerObj.hp = attackerObj.hp - defenderObj.counterattack;
+
+        ScreenUpdate(attackerObj, defenderObj);
+
+         //attacker attack increases by attack power
+        attackerObj.attack += attackerObj.baseattack;
+        console.log(attackerObj.attack)
+
+        var jsonstring = JSON.stringify(attackerObj);
+        $(".selected").attr("data-obj", jsonstring)
+        console.log(jsonstring)
+        jsonstring = JSON.stringify(defenderObj);
+        $(".defender").attr("data-obj", jsonstring)
+        console.log(jsonstring)
+
+    }
+    else
+    {
+        $("#pAttacker").text("No enemy here.")
+        $("#pDefender").text("");
+    }
 
 });
 
-function GetJSONString(htmlid){
-
-    for(var i= 0; i < Object.keys(characters).length; i++)
+function GetJSONString(htmlid)
+{
+    for (var i = 0; i < Object.keys(characters).length; i++)
     {
         var key = (Object.keys(characters)[i]);
         idkey = characters[key].id;
-       //console.log("ck= " + JSON.stringify(characters[key]));      
-        if(idkey == htmlid)
+
+        if (idkey == htmlid)
         {
             var jsonstring = JSON.stringify(characters[key]);
             return jsonstring;
         }
     }
-    
 }
 
+
+function ScreenUpdate(attackerObj, defenderObj)
+{
+
+    if (attackerObj.hp != 0 && defenderObj.hp != 0)
+    {
+        //Display message below Deender
+        $("#pAttacker").text("You attacked " + defenderObj.name + " for " + attackerObj.attack + " damage");
+        $("#pDefender").text(defenderObj.name + "Attacked you back for " + defenderObj.attack + " damage");
+
+        //Update HP
+        $(".selected #hp").text(attackerObj.hp)
+        $(".defence #hp").text(defenderObj.hp)
+    }
+    else if (attackerObj.hp == 0 && defenderObj.hp != 0)
+    {
+        $(".selected #hp").text(attackerObj.hp)
+        $(".defence #hp").text(defenderObj.hp)
+        $("#pAttacker").text("You've Been Defeated....GAME OVER")
+        $("#pDefender").text("");
+        //Create or show button to restart
+    }
+    else if (attackerObj.hp != 0 && defenderObj.hp == 0)
+    {
+        $(".defence").remove();
+
+        if ($(".enemies").children().length >= 0)
+        {
+            $("#pDefender").empty();
+            $("#pAttacker").text("You have defeated" + defenderObj.name + ", You can choose to fight another enemie");
+
+        }
+        else
+        {
+            $("#pAttacker").text("YOU WON!!!!GAME OVER!!!")
+            $("#pDefender").text("");
+            //Create or show button to restart
+
+        }
+
+
+
+    }
+
+
+}
